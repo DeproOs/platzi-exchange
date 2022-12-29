38f3c244c1155df4a97b2868f6575c6256f4fbad
DeproOs <klaudio_4ever@hotmail.com>
2022-12-29 10:26:45 -0300

    se agregan views para mostrar en tiempo real

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <px-assets-table></px-assets-table>
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable.vue";
+export default {
+  // eslint-disable-next-line
+  name: "Home",
+  components: { PxAssetsTable },
+};
+</script>
